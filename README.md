a0a504ad636b78d943a6c3505cac9ebe2f1d52d6
david <david@hamlyn>
2022-07-30 16:52:48 +0100

    Help and about info for command.

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # VisualStudioNugetPushFix
 
 Working publish command for the visual studio artifact feed.
+
+## About
+
+This module is to allow publishing to authenticated nuget feeds.
+As of the writing of this module, powershell's nuget implementation does not allow publishing to authenticated nuget v2 without workarounds or to v3 feeds at all.
+This module implements the API for the nuget feed directly instead of using nuget.exe or nuget.*.dll libraries.
+
+This module only deals with publishing modules. For installing modules, you should still use the PowershellGet preview 3.0 or newer module which does support installing from a v3 feed.
+
+## Commands
+
+### Publish-VisualStudioNuGetV3FeedFile
+
+Publish a file to a nuget feed.
+This should work for the artifact feeds that are hosted on visualstudio.com.
+It might also work for other feeds such as github, but they are not a test target at the moment.
+
+## Author
+
+PurpleMonkeyMad  
+github.com/purplemonkeymad  
+/u/purplemonkemad
--- VisualStudioNugetPushFix/Public/Publish-VisualStudioNugetv3Feed.ps1
+++ VisualStudioNugetPushFix/Public/Publish-VisualStudioNugetv3Feed.ps1
@@ -1,3 +1,49 @@
+<#
+
+.Synopsis
+Push nuget files to a Artifact feed from VisualStudio dev ops.
+
+.Description
+Pushes a nuget file to a nuget v3 feed. The command targets the api requirements for the artifact feeds you get with MS dev ops hosted at visualstudio.
+
+At time of creatation the official nuget libraries don't allow powershell to push to a v3 feed using authentication. Basic Auth is needed on these feeds. 
+
+May also work on other authenticated v3 feeds.
+
+.Parameter Credential
+Username and password to authenticate with. Should be the same ones when used with the Find/Install-PSResource commands. Typically for VisualStudio devops, this is you PAT and any username, other nuget feeds might require a matching username.
+
+.Parameter Feed
+The name of the Atrifact feed. Needed and required only when using the Organisation parameter. This is sypically the named folder of the feed, ie ../_artifacts/feed/<feedname>/ or ../_packaging/<feedname>/nuget/v3/index.json
+
+.Parameter File
+Path to file to be uploaded. Should be a .nuget file for best results.
+
+.Parameter Organisation
+The name of the organisation hosting the feed. This is typically the subdomain of the feed, ie: <orgname>.visualstudio.com/
+
+.Parameter PAT
+You Personal Access Token. This is only needed if you have a different publish PAT from your normal retrival PAT. If not specified then the password from the credential parameter will be used as the publish PAT.
+
+.Parameter Repository
+PSResourceRepository to publish to. The repository should be a v3 feed, v3 feeds should end with `index.json`.
+
+.Parameter Uri
+Direct URI of the feed to publish to. This should be the service index not the publish endpoint.
+
+.Example
+Publish-VisualStudioNugetv3FeedFile -Credential <PSCredential> -File module.0.0.1.nuget -Uri https://mynugetfeed.example.com/index.json
+This will directly publish a file to the given feed. The Uri should be the service index of the nuget feed.
+
+.Example
+Publish-VisualStudioNugetv3FeedFile -Credential <PSCredential> -File module.0.0.1.nuget -Repository myrepo
+Will retrive the repository from the PowershellGet repository list called myrepo. The feed uri can been seen using `Get-PSResourceRepository myrepo`.
+
+.Example
+Publish-VisualStudioNugetv3FeedFile -Credential <PSCredential> -Organisation <String> -Feed <String> -File <String>
+Will publish to the given Organisation and Feed names in a VisualStudio hosted Artifact feed.
+
+#>
 function Publish-VisualStudioNugetv3FeedFile {
     [CmdletBinding(DefaultParameterSetName="URI")]
     Param(
